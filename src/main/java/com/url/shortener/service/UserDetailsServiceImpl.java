9c160170e12bbaac14db0cb56f51274eaa41e8d4
ApurbaRajyashree <apurbarajyashreethapa@gmail.com>
2026-04-24 21:34:18 +0100

    Spring Security Configuration completed

---
+++ src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.url.shortener.service;
+
+import com.url.shortener.model.User;
+import com.url.shortener.repo.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User with username: " + username + " not found!")
+        );
+        return UserDetailsImpl.build(user);
+    }
+}
